afbd0cb56d655f24b9dfccb4df97e29a1e64c86c
Piotr <piot.domzalski91@gmail.com>
2022-02-26 15:35:55 +0100

    writing and adding a new page

--- src/main/java/PdfBoxExample.java
+++ src/main/java/PdfBoxExample.java
@@ -1,9 +1,27 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import java.util.Calendar;
 
 public class PdfBoxExample {
+
+    public static void writeLineOfText(PDPageContentStream contentStream, int tx, int ty, PDFont font, String text) {
+        if (font == null) font = PDType1Font.HELVETICA;
+
+        try {
+            contentStream.beginText();
+            contentStream.setFont(font, 12);
+            contentStream.newLineAtOffset(tx, ty); // 595 x 842
+            contentStream.showText(text);
+            contentStream.endText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         //Tworzenie nazwy pliku i jego ścieżka // Creating a file name and its path
         Calendar c = Calendar.getInstance();
@@ -13,6 +31,21 @@ public class PdfBoxExample {
         PDDocument document = new PDDocument();
         PDPage page1 = new PDPage();
         document.addPage(page1);
+
+        PDPageContentStream contentStream1 = new PDPageContentStream(document, page1);
+
+        writeLineOfText(contentStream1, 40, 100, null, "Hello World!");
+
+        contentStream1.close();
+
+        // nowa strona
+        PDPage page2 = new PDPage();
+        PDPageContentStream contentStream2 = new PDPageContentStream(document, page2);
+        document.addPage(page2);
+
+        writeLineOfText(contentStream2, 40, 700, PDType1Font.COURIER_BOLD, "Hello World!");
+
+        contentStream2.close();
 
         document.save(fileName);
         document.close();
